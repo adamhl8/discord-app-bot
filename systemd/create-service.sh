208fdacb03c79b16ee7bb5698dfb6e520f42069c
Adam Langbert <adamhl@pm.me>
2021-08-07 00:41:03 +0200

    fix typo

--- systemd/create-service.sh
+++ systemd/create-service.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo cp discord-app-bot.service /etc/systemd/system/
 sudo systemctl daemon-reload
-sudo systemtl enable discord-app-bot
+sudo systemctl enable discord-app-bot
 sudo systemctl start discord-app-bot
 sudo systemctl status discord-app-bot
