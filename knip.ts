a74c948f324ea6f6294a89870f49ec7b3f49a8ee
Adam Langbert <adamhl@pm.me>
2026-07-02 15:41:41 -0500

    chore: fix knip prisma config

--- knip.ts
+++ knip.ts
@@ -3,6 +3,12 @@ import { knipConfig } from "@adamhl8/configs"
 const config = knipConfig({
   project: ["!./src/generated/**/*"],
   ignoreDependencies: ["@prisma/client"],
-} as const)
+  prisma: {
+    // By default, knip loads the `prisma.config.ts` file, which in turns tries to get the database URL: `env("DATABASE_URL")`.
+    // This fails in CI where DATABASE_URL is not set. So instead we stop knip from loading the config and set the entries manually.
+    config: [],
+    entry: ["prisma.config.ts", "prisma/schema.prisma"],
+  },
+})
 
 export default config
